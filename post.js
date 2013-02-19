2d7afe7c7f1bb4520e69c6b101d135851391c62d
Andreas Krennmair <ak@synflood.at>
2013-02-19 17:16:23 +0100

    add more functions to parametrize mp3 output.

--- post.js
+++ post.js
@@ -1,14 +1,36 @@
 // libmp3lame function wrappers
 
+var BUFSIZE = 8192;
+
 return {
+	STEREO: 0, 
+	JOINT_STEREO: 1, 
+	MONO: 3,
+
 	get_version: Module.cwrap('get_lame_version', 'string'),
 
 	init: Module.cwrap('lame_init', 'number'),
-
 	init_params: Module.cwrap('lame_init_params', 'number', [ 'number' ]),
 
+	set_mode: Module.cwrap('lame_set_mode', 'number', [ 'number', 'number' ]),
+	get_mode: Module.cwrap('lame_get_mode', 'number', [ 'number' ]),
+
+	set_num_samples: Module.cwrap('lame_set_num_samples', 'number', [ 'number', 'number' ]),
+	get_num_samples: Module.cwrap('lame_get_num_samples', 'number', [ 'number' ]),
+
+	set_num_channels: Module.cwrap('lame_set_num_channels', 'number', [ 'number', 'number' ]),
+	get_num_channels: Module.cwrap('lame_get_num_channels', 'number', [ 'number' ]),
+
+	set_in_samplerate: Module.cwrap('lame_set_in_samplerate', 'number', [ 'number', 'number' ]),
+	get_in_samplerate: Module.cwrap('lame_get_in_samplerate', 'number', [ 'number' ]),
+
+	set_out_samplerate: Module.cwrap('lame_set_out_samplerate', 'number', [ 'number', 'number' ]),
+	get_out_samplerate: Module.cwrap('lame_get_out_samplerate', 'number', [ 'number' ]),
+
+	set_bitrate: Module.cwrap('lame_set_brate', 'number', [ 'number', 'number' ]),
+	get_bitrate: Module.cwrap('lame_get_brate', 'number', [ 'number' ]),
+
 	encode_buffer_ieee_float: function(handle, channel_l, channel_r) {
-		var BUFSIZE = 8192;
 		var outbuf = _malloc(BUFSIZE);
 		var inbuf_l = _malloc(channel_l.length * 4);
 		var inbuf_r = _malloc(channel_r.length * 4);
@@ -29,7 +51,6 @@ return {
 	},
 
 	encode_flush: function(handle) {
-		var BUFSIZE = 8192;
 		var outbuf = _malloc(BUFSIZE);
 		var nread = Module.ccall('lame_encode_flush', 'number', [ 'number', 'number', 'number' ], [ handle, outbuf, BUFSIZE ]);
 		var arraybuf = new ArrayBuffer(nread);
