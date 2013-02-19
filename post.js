f3f8b80d5483a54d9a83b319f14a74a460948c17
Andreas Krennmair <ak@synflood.at>
2013-02-19 14:44:05 +0100

    back Uint8Array with ArrayBuffer.

--- post.js
+++ post.js
@@ -19,7 +19,8 @@ return {
 			setValue(inbuf_r + (i*4), channel_r[i], 'float');
 		}
 		var nread = Module.ccall('lame_encode_buffer_ieee_float', 'number', [ 'number', 'number', 'number', 'number', 'number', 'number' ], [ handle, inbuf_l, inbuf_r, channel_l.length, outbuf, BUFSIZE ]);
-		var retdata = new Uint8Array(nread);
+		var arraybuf = new ArrayBuffer(nread);
+		var retdata = new Uint8Array(arraybuf);
 		retdata.set(HEAPU8.subarray(outbuf, outbuf + nread));
 		_free(outbuf);
 		_free(inbuf_l);
@@ -31,7 +32,8 @@ return {
 		var BUFSIZE = 8192;
 		var outbuf = _malloc(BUFSIZE);
 		var nread = Module.ccall('lame_encode_flush', 'number', [ 'number', 'number', 'number' ], [ handle, outbuf, BUFSIZE ]);
-		var retdata = new Uint8Array(nread);
+		var arraybuf = new ArrayBuffer(nread);
+		var retdata = new Uint8Array(arraybuf);
 		retdata.set(HEAPU8.subarray(outbuf, outbuf + nread));
 		_free(outbuf);
 		return { size: nread, data: retdata };
